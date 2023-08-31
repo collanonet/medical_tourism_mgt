29eda6112fcd350bb2d555e28ddc6fc5e6d37440
Mengthong <mengthong.oeng@jaksmok.com>
2023-08-31 22:05:21 +0700

    add core_l10n for change languages

---
+++ modules/core_l10n/lib/l10n.dart
@@ -0,0 +1,3 @@
+export 'l10n/app_localizations.dart';
+export 'src/extensions/context_exts.dart';
+export 'src/l10n_model.dart';
---
+++ modules/core_l10n/lib/src/l10n_model.dart
@@ -0,0 +1,48 @@
+import 'dart:ui';
+
+import 'package:core_storage/core_storage.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:injectable/injectable.dart';
+
+@injectable
+class L10nModel extends ChangeNotifier {
+  static const _languageCodeStorageKey = 'language_code';
+  static const _countryCodeStorageKey = 'country_code';
+
+  L10nModel({
+    @Named('defaultLocale') required this.defaultLocale,
+    @Named('localeStorage') required this.storage,
+  });
+
+  final Locale defaultLocale;
+  final Storage storage;
+
+  Locale? _locale;
+  Locale get locale => _locale ?? defaultLocale;
+
+  Future<void> initialize() async {
+    try {
+      _locale = await Future.wait([
+        storage.read(_languageCodeStorageKey),
+        storage.read(_countryCodeStorageKey),
+      ]).then((values) {
+        return values[0] != null ? Locale(values[0], values[1]) : null;
+      });
+    } catch (e) {
+      _locale = defaultLocale;
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  Future<void> setLocale(Locale locale) {
+    _locale = locale;
+    notifyListeners();
+
+    return Future.wait([
+      storage.write(_languageCodeStorageKey, locale.languageCode),
+      storage.write(_countryCodeStorageKey, locale.countryCode)
+    ]);
+  }
+}
