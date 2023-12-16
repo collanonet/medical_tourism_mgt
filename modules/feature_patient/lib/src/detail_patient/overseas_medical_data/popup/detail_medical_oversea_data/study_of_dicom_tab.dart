940c04b45f79a20bd0d2efbd2d7dba3455bbadbc
Mengthong <mengthongoenggl@gmail.com>
2023-12-17 00:24:07 +0700

    finsih tab 国内診療データ in detail patient

--- modules/feature_patient/lib/src/detail_patient/overseas_medical_data/popup/detail_medical_oversea_data/study_of_dicom_tab.dart
+++ modules/feature_patient/lib/src/detail_patient/overseas_medical_data/popup/detail_medical_oversea_data/study_of_dicom_tab.dart
@@ -1,3 +1,4 @@
+import 'package:core_ui/core_ui.dart';
 import 'package:flutter/material.dart';
 
 class StudyOfDICOMTab extends StatelessWidget {
@@ -5,6 +6,23 @@ class StudyOfDICOMTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              ElevatedButton(onPressed: null, child: Text('001-C-20')),
+              SizedBox(
+                width: context.appTheme.spacing.marginMedium,
+              ),
+              ElevatedButton(onPressed: null, child: Text('王　沐宸')),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
