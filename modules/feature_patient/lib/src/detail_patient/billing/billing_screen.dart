d435bd8335561e602748c321d3a0b55991a1e386
Mengthong <mengthongoenggl@gmail.com>
2023-12-17 01:00:43 +0700

    working on tab 精算履歴 at detail patient

--- modules/feature_patient/lib/src/detail_patient/billing/billing_screen.dart
+++ modules/feature_patient/lib/src/detail_patient/billing/billing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:core_ui/core_ui.dart';
 import 'package:flutter/material.dart';
 
 class BillingScreen extends StatefulWidget {
@@ -10,6 +11,125 @@ class BillingScreen extends StatefulWidget {
 class _BillingScreenState extends State<BillingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(
+              context.appTheme.spacing.marginSmall,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(
+                context.appTheme.spacing.borderRadiusMedium,
+              )),
+              color: context.appTheme.primaryColor.withOpacity(0.1),
+              border: Border.all(
+                color: context.appTheme.primaryColor,
+              ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  '預かり金（治療費）',
+                  style: context.textTheme.titleMedium,
+                ),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Expanded(child: Text('日本円（税込）')),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          '6,000,000円',
+                          style: context.textTheme.titleLarge,
+                        ))),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: context.appTheme.spacing.marginMedium,
+          ),
+          Container(
+            padding: EdgeInsets.all(
+              context.appTheme.spacing.marginSmall,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(
+                context.appTheme.spacing.borderRadiusMedium,
+              )),
+              color: context.appTheme.primaryColor.withOpacity(0.1),
+              border: Border.all(
+                color: context.appTheme.primaryColor,
+              ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  '精算金（診療報酬明細書）',
+                  style: context.textTheme.titleMedium,
+                ),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Expanded(child: Text('日本円（税込）')),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          '4,590,000円',
+                          style: context.textTheme.titleLarge,
+                        ))),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: context.appTheme.spacing.marginMedium,
+          ),
+          Container(
+            padding: EdgeInsets.all(
+              context.appTheme.spacing.marginSmall,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(
+                context.appTheme.spacing.borderRadiusMedium,
+              )),
+              color: Colors.yellow.withOpacity(0.1),
+              border: Border.all(
+                color: Colors.yellow,
+              ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  '残金',
+                  style: context.textTheme.titleMedium,
+                ),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Expanded(child: Text('日本円（税込）')),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          '1,410,000円',
+                          style: context.textTheme.titleLarge,
+                        ))),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
