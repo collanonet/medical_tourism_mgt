71327da6ecd6f6b32d426d747ac064adf4bacf8e
Mengthong <mengthongoenggl@gmail.com>
2023-12-20 00:11:47 +0700

    finish UI menu Web予約

--- modules/feature_web_appointment/lib/feature_web_appointment.module.dart
+++ modules/feature_web_appointment/lib/feature_web_appointment.module.dart
@@ -1,13 +1,13 @@
 //@GeneratedMicroModule;FeatureWebAppointmentPackageModule;package:feature_web_appointment/feature_web_appointment.module.dart
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unnecessary_lambdas
-// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: type=lint
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i2;
 
 import 'package:data_auth/data_auth.dart' as _i4;
+import 'package:data_patient/data_patient.dart' as _i5;
 import 'package:injectable/injectable.dart' as _i1;
 
 import 'src/web_appointment_model.dart' as _i3;
@@ -16,7 +16,9 @@ class FeatureWebAppointmentPackageModule extends _i1.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
   _i2.FutureOr<void> init(_i1.GetItHelper gh) {
-    gh.factory<_i3.WebAppointmentModel>(() =>
-        _i3.WebAppointmentModel(authRepository: gh<_i4.AuthRepository>()));
+    gh.factory<_i3.WebAppointmentModel>(() => _i3.WebAppointmentModel(
+          authRepository: gh<_i4.AuthRepository>(),
+          patientRepository: gh<_i5.PatientRepository>(),
+        ));
   }
 }
--- modules/feature_web_appointment/lib/src/filter_web_appointment_form.dart
+++ modules/feature_web_appointment/lib/src/filter_web_appointment_form.dart
@@ -2,29 +2,17 @@ import 'package:core_l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
-FormGroup formFilterPatient() => FormGroup(
+FormGroup formFilterWebAppointment() => FormGroup(
       {
-        'patient_name': FormControl<String>(
-          value: '',
-        ),
-        'agent_company_name': FormControl<String>(
-          value: '',
-        ),
         'hospital_name': FormControl<String>(
           value: '',
         ),
-        'classification': FormControl<String>(
+        'doctor_name': FormControl<String>(
           value: '',
         ),
-        'sales_representative': FormControl<String>(
-          value: '',
-        ),
-        'entry_date_from': FormControl<DateTime>(),
-        'entry_date_to': FormControl<DateTime>(),
-        'examination_date_from': FormControl<DateTime>(),
-        'examination_date_to': FormControl<DateTime>(),
-        'return_date_from': FormControl<DateTime>(),
-        'return_date_to': FormControl<DateTime>(),
+        'reservation_date_from': FormControl<DateTime>(),
+        'reservation_date_to': FormControl<DateTime>(),
+        'reservations_status': FormControl<String>(),
       },
     );
 
