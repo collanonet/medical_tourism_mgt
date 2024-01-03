84531bc33a6a803362f37301ae6196b15e0e6069
Mengthong <mengthongoenggl@gmail.com>
2024-01-03 23:46:00 +0700

    working on E2-E6

--- modules/feature_process_chart/lib/src/detail/tab/facility_form.dart
+++ modules/feature_process_chart/lib/src/detail/tab/facility_form.dart
@@ -1,9 +1,13 @@
 import 'package:reactive_forms/reactive_forms.dart';
 
 FormGroup facilityForm() => FormGroup(
-  {
-    'patient_name': FormControl<String>(
-      value: '',
-    ),
-  },
-);
+      {
+        '手配担当': FormControl<String>(value: ''), // 手配担当
+        '施設名': FormControl<String>(value: ''), // 施設名
+        '所在地': FormControl<String>(value: ''), // 所在地
+        '担当者名': FormControl<String>(value: ''), // 担当者名
+        '電話番号': FormControl<String>(value: ''), // 電話番号
+        '備考': FormControl<String>(value: ''), // 備考
+        '外国語スタッフ': FormControl<String>(value: ''), // 外国語スタッフ
+      },
+    );
--- modules/feature_process_chart/lib/src/detail/tab/itinerary_form.dart
+++ modules/feature_process_chart/lib/src/detail/tab/itinerary_form.dart
@@ -2,8 +2,59 @@ import 'package:reactive_forms/reactive_forms.dart';
 
 FormGroup itineraryForm() => FormGroup(
       {
-        'patient_name': FormControl<String>(
-          value: '',
+        'patient_names': FormArray(
+          [
+            FormGroup(
+              {
+                'patient_name': FormControl<String>(value: ''), // 患者名
+              },
+            ),
+          ],
+        ),
+        'tour name': FormControl<String>(value: ''), // ツアー名
+        'Number of people': FormControl<String>(value: ''), // 人数
+        'group': FormControl<String>(value: ''), // グループ
+        'type': FormControl<String>(value: ''), // 種別
+        'day': FormArray(
+          // 日目
+          [
+            FormGroup(
+              {
+                'date': FormControl<String>(value: ''), // 日付
+                'meal': FormControl<String>(value: ''), // 食事
+                'place name': FormControl<String>(value: ''), // 地名
+                'Accommodation': FormControl<String>(value: ''), // 宿泊場所
+                'group': FormArray(
+                  // グループ
+                  [
+                    FormGroup(
+                      {
+                        'group_number':
+                            FormControl<String>(value: ''), // グループ番号
+                        'task': FormArray(
+                          [
+                            FormGroup(
+                              {
+                                'place name':
+                                    FormControl<String>(value: ''), // 地名
+                                'Time (from)':
+                                    FormControl<String>(value: ''), // 時刻（自）
+                                'Time (to)':
+                                    FormControl<String>(value: ''), // 時刻（至）
+                                'traffic': FormControl<String>(value: ''), // 交通
+                                'Itinerary':
+                                    FormControl<String>(value: ''), // 行程
+                              },
+                            ),
+                          ],
+                        ),
+                      },
+                    ),
+                  ],
+                ),
+              },
+            ),
+          ],
         ),
       },
     );
