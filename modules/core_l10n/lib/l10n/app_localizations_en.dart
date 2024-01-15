529bfb0b3ef4839b6998f70722515d88ee04f211
Oeng Mengthong <mengthongoenggl@gmail.com>
2024-01-15 21:59:52 +0700

    update

---
+++ modules/core_l10n/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,198 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get titleAppJP => 'センター本部専用';
+
+  @override
+  String get errorUnknown => '何かが間違っていた。';
+
+  @override
+  String get errorTimeout => '時間切れ';
+
+  @override
+  String get errorConnection => '接続の問題';
+
+  @override
+  String get labelId => 'ID';
+
+  @override
+  String get labelPassword => 'パスワード';
+
+  @override
+  String get labelPleaseEnterYourInformation => '入力してください';
+
+  @override
+  String get labelForgotPassword => 'パスワードをお忘れの方はこちら＞';
+
+  @override
+  String get mgsFieldRequired => 'フィールドは必須';
+
+  @override
+  String get actionLogin => 'ログイン';
+
+  @override
+  String get mgsRequireInputLogin => 'アカウントを入力してください';
+
+  @override
+  String get labelPatient => 'Patient';
+
+  @override
+  String get labelChat => 'Chat';
+
+  @override
+  String get labelMedicalVisaManagement => 'Medical Visa';
+
+  @override
+  String get labelWebAppointment => 'Web Reservation';
+
+  @override
+  String get labelProcessChart => 'Process Chart';
+
+  @override
+  String get labelHospitals => 'Hospitals';
+
+  @override
+  String get labelAgents => 'Agent';
+
+  @override
+  String get labelQuotations => 'Quotations';
+
+  @override
+  String get labelInvoice => 'Invoice';
+
+  @override
+  String get labelSalesManagement => 'Sales';
+
+  @override
+  String get labelReport => 'Report';
+
+  @override
+  String get labelHelp => 'Help';
+
+  @override
+  String get labelCenterHeadquarters => 'Center Headquarters';
+
+  @override
+  String get labelPatientSearch => '患者検索';
+
+  @override
+  String get labelPatientName => '患者氏名';
+
+  @override
+  String get labelAgentCompanyName => 'エージェント会社名';
+
+  @override
+  String get labelHospitalName => '病院名';
+
+  @override
+  String get labelClassification => '種別';
+
+  @override
+  String get labelSalesRepresentative => '営業担当者';
+
+  @override
+  String get labelEntryDateFrom => '入国日（自）';
+
+  @override
+  String get labelEntryDateTo => '入国日（至）';
+
+  @override
+  String get labelExaminationDateFrom => '受診日（自）';
+
+  @override
+  String get labelExaminationDateTo => '受診日（至）';
+
+  @override
+  String get labelReturnDateFrom => '帰国日（自）';
+
+  @override
+  String get labelReturnDateTo => '帰国日（至）';
+
+  @override
+  String get actionClear => 'クリア';
+
+  @override
+  String get actionSearch => '検索';
+
+  @override
+  String get actionListOfPreMembers => 'プレ会員一覧';
+
+  @override
+  String get actionNewRegistration => '新規登録';
+
+  @override
+  String get labelOrdersOnly => '受注のみ';
+
+  @override
+  String get labelAll => 'すべて';
+
+  @override
+  String get labelCXL => 'CXL';
+
+  @override
+  String get labelAgentName => 'エージェント名';
+
+  @override
+  String get labelAgent => 'エージェント';
+
+  @override
+  String get labelDateOfBirth => '生年月日';
+
+  @override
+  String get labelGender => '性別';
+
+  @override
+  String get labelNationality => '国籍';
+
+  @override
+  String get labelNameOfaDisease => '病名';
+
+  @override
+  String get labelWeChatPrePatientSearch => 'WeChatプレ患者検索';
+
+  @override
+  String get actionGoToRegister => '本登録へ';
+
+  @override
+  String get actionDelete => '削除する';
+
+  @override
+  String get actionDeleted => '削除済み';
+
+  @override
+  String get labelProgress => '進捗';
+
+  @override
+  String get labelCaseNumber => '案件番号';
+
+  @override
+  String get labelEntryDate => '入国日';
+
+  @override
+  String get labelExaminationDate => '診療日';
+
+  @override
+  String get labelReturnDate => '帰国日';
+
+  @override
+  String get labelSale => '営業';
+
+  @override
+  String get labelStaffName => '業務';
+
+  @override
+  String get labelAdmittedToHospital => '受入病院';
+
+  @override
+  String get labelNumberOfGroupMembers => 'グループ人数';
+
+  @override
+  String get labelLogout => 'ログアウト';
+
+  @override
+  String get changeLanguage => 'Please select a language';
+}
