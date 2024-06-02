b5678e9d985d29bb4af6e728101dc99656258d32
Mengthong <mengthongoenggl@gmail.com>
2024-06-02 12:29:04 +0700

    update build

--- modules/feature_patient/lib/src/detail_patient/overseas_medical_data/overseas_medical_data_model.dart
+++ modules/feature_patient/lib/src/detail_patient/overseas_medical_data/overseas_medical_data_model.dart
@@ -102,15 +102,9 @@ class OverseasMedicalDataModel {
       final token = await GetIt.I<AuthRepository>().getAccessToken();
       logger.d("token: $token");
       createMedicalOverseaData.value = const AsyncData(loading: true);
+
       String? file;
       if (formGroup.control('file').value != null) {
-        // try {
-        //   file = await patientRepository.uploadFileDio(
-        //       formGroup.control('file').value, token ?? '');
-        // } catch (e) {
-        //   logger.e(e);
-        // }
-
         try {
           // convert Uint8List to base64
           FileSelect docFile = formGroup.control('file').value;
@@ -128,13 +122,6 @@ class OverseasMedicalDataModel {
       String? qrCode;
 
       if (formGroup.control('qrCode').value != null) {
-        // try {
-        //   qrCode = await patientRepository.uploadFileDio(
-        //       formGroup.control('qrCode').value, token ?? '');
-        // } catch (e) {
-        //   logger.e(e);
-        // }
-
         try {
           // convert Uint8List to base64
           FileSelect qrFile = formGroup.control('qrCode').value;
@@ -149,8 +136,6 @@ class OverseasMedicalDataModel {
         }
       }
 
-      logger.d("file: $file");
-      logger.d("qrCode: $qrCode");
       var medicalRecordOverseaDataRequest = MedicalRecordOverseaDataRequest(
         file: file,
         hospitalName: formGroup.control('hospitalName').value,
