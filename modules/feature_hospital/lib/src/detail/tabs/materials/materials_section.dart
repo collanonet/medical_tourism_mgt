62dd0a1603cd48735350e0033b93078e023581e8
Mengthong <mengthongoenggl@gmail.com>
2024-07-15 23:16:51 +0700

    fix upload and list file for material at detail hospital

--- modules/feature_hospital/lib/src/detail/tabs/materials/materials_section.dart
+++ modules/feature_hospital/lib/src/detail/tabs/materials/materials_section.dart
@@ -26,256 +26,242 @@ class MaterialSectionState extends State<MaterialSection> {
   @override
   Widget build(BuildContext context) {
     final formGroup = ReactiveForm.of(context) as FormGroup;
-    return SingleChildScrollView(
-      child: ColumnSeparated(
-        separatorBuilder: (BuildContext context, int index) =>
-            SizedBox(height: context.appTheme.spacing.formSpacing),
-        children: [
-          // Memo
-          ValueListenableBuilder(
-            valueListenable: context.read<MaterialsModel>().memoMaterialsData,
-            builder: (context, value, _) {
-              return Skeletonizer(
-                enabled: value.loading,
-                child: Column(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'メモ',
-                        ),
-                        ReactiveTextField(
-                          formControlName: 'memo',
-                          maxLines: 6,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        // Make as a copy button
-                        ElevatedButton(
-                          onPressed: () {
-                            // todo: copy memo to clipboard
-                          },
-                          child: const Text(
-                            'コピーする',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: context.appTheme.spacing.marginMedium,
-                        ),
-                        // Save button
-                        ElevatedButton(
-                          onPressed: () {
-                            context
-                                .read<MaterialsModel>()
-                                .submitMemoData(formGroup);
-                          },
-                          child: const Text(
-                            '保存する',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-          //Drag and drop file
-          InkWell(
-            onTap: () {
-              filePicker().then((value) {
-                if (value != null) {
-                  showCreateWithFileDialog(context, value);
-                }
-              });
-            },
-            child: Container(
-              padding: EdgeInsets.all(
-                context.appTheme.spacing.marginExtraLarge,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(
-                  context.appTheme.spacing.borderRadiusMedium,
-                )),
-                border: Border.all(
-                  color: context.appTheme.primaryColor,
-                ),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+    return ColumnSeparated(
+      separatorBuilder: (BuildContext context, int index) =>
+          SizedBox(height: context.appTheme.spacing.formSpacing),
+      children: [
+        // Memo
+        ValueListenableBuilder(
+          valueListenable: context.read<MaterialsModel>().memoMaterialsData,
+          builder: (context, value, _) {
+            return Skeletonizer(
+              enabled: value.loading,
+              child: Column(
                 children: [
-                  Icon(
-                    Icons.copy_all_rounded,
-                    size: 50,
-                    color: context.appTheme.primaryColor,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'メモ',
+                      ),
+                      ReactiveTextField(
+                        formControlName: 'memo',
+                        maxLines: 6,
+                      ),
+                    ],
                   ),
                   SizedBox(
-                    width: context.appTheme.spacing.marginMedium,
+                    height: context.appTheme.spacing.marginMedium,
                   ),
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Text(
-                        '診療データをここにドラッグ＆ドロップ',
-                        style: context.textTheme.bodySmall?.copyWith(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
+                      // Make as a copy button
+                      ElevatedButton(
+                        onPressed: () {
+                          // todo: copy memo to clipboard
+                        },
+                        child: const Text(
+                          'コピーする',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                       SizedBox(
-                        height: context.appTheme.spacing.marginMedium,
+                        width: context.appTheme.spacing.marginMedium,
                       ),
+                      // Save button
                       ElevatedButton(
                         onPressed: () {
-                          filePicker().then((value) {
-                            if (value != null) {
-                              showCreateWithFileDialog(context, value);
-                            }
-                          });
+                          context
+                              .read<MaterialsModel>()
+                              .submitMemoData(formGroup);
                         },
                         child: const Text(
-                          'またはファイルを選択する',
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-
-          SizedBox(
-            height: 500,
-            child: DataTable2(
-              columnSpacing: 8,
-              horizontalMargin: 16,
-              minWidth: 450,
-              dataRowHeight: 70,
-              isVerticalScrollBarVisible: true,
-              isHorizontalScrollBarVisible: true,
-              showCheckboxColumn: true,
-              onSelectAll: (bool? value) {},
-              datarowCheckboxTheme: CheckboxThemeData(
-                checkColor: MaterialStateProperty.resolveWith(
-                    (states) => context.appTheme.primaryColor),
-              ),
-              headingTextStyle: const TextStyle(
-                  fontFamily: 'NotoSansJP',
-                  package: 'core_ui',
-                  color: Colors.grey),
-              dividerThickness: 0,
-              columns: [
-                ...[
-                  'パンフレット名',
-                  '作成者',
-                  '発行日',
-                  '共有',
-                ].map((e) => DataColumn2(
-                      label: Text(
-                        e,
-                        style: context.textTheme.bodySmall,
-                      ),
-                    )),
-              ],
-              rows: List.generate(6, (index) {
-                return DataRow2(
-                  selected: false,
-                  onSelectChanged: (value) => debugPrint('row selected'),
-                  cells: [
-                    const DataCell(Text('パンフレット名')),
-                    DataCell(Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 4),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.blue),
-                              borderRadius: BorderRadius.circular(4)),
-                          child: const Text(
-                            '作成者',
-                            style: TextStyle(
-                                fontFamily: 'NotoSansJP',
-                                package: 'core_ui',
-                                color: Colors.blue),
+                          '保存する',
+                          style: TextStyle(
+                            color: Colors.white,
                           ),
                         ),
-                      ],
-                    )),
-                    const DataCell(Text('2021-09-01')),
-                    const DataCell(Icon(Icons.person)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        InkWell(
+          onTap: () {
+            filePicker().then((value) {
+              if (value != null) {
+                showCreateWithFileDialog(context, value);
+              }
+            });
+          },
+          child: Container(
+            padding: EdgeInsets.all(
+              context.appTheme.spacing.marginExtraLarge,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(
+                context.appTheme.spacing.borderRadiusMedium,
+              )),
+              border: Border.all(
+                color: context.appTheme.primaryColor,
+              ),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.copy_all_rounded,
+                  size: 50,
+                  color: context.appTheme.primaryColor,
+                ),
+                SizedBox(
+                  width: context.appTheme.spacing.marginMedium,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '診療データをここにドラッグ＆ドロップ',
+                      style: context.textTheme.bodySmall?.copyWith(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: context.appTheme.spacing.marginMedium,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        filePicker().then((value) {
+                          if (value != null) {
+                            showCreateWithFileDialog(context, value);
+                          }
+                        });
+                      },
+                      child: const Text(
+                        'またはファイルを選択する',
+                      ),
+                    )
                   ],
-                );
-              }).toList(),
+                )
+              ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: context.appTheme.spacing.marginSmall,
-                        vertical: context.appTheme.spacing.buttonVertical,
+        ),
+        const Row(
+          children: [
+            Expanded(flex: 2, child: Text('パンフレット名')),
+            Expanded(child: Text('作成者')),
+            Expanded(child: Text('発行日')),
+            Expanded(child: Text('共有')),
+          ],
+        ),
+        ValueListenableBuilder(
+            valueListenable: context.read<MaterialsModel>().materialsData,
+            builder: (context, value, _) {
+              return Expanded(
+                child: ListView.separated(
+                  itemCount: value.data?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                                value.requireData[index].brochureName ?? ''),
+                          ),
+                          Expanded(
+                            child: Text(value.requireData[index].author ?? ''),
+                          ),
+                          Expanded(
+                            child: Text(
+                                value.requireData[index].dateOfIssue == null
+                                    ? ''
+                                    : Dates.formShortDate(
+                                        value.requireData[index].dateOfIssue)),
+                          ),
+                          Expanded(
+                            child: value.requireData[index].share == null
+                                ? SizedBox()
+                                : Icon(Icons.person),
+                          ),
+                        ],
                       ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  onPressed: () {},
-                  child: Text(
-                    "削除する",
-                    style: context.textTheme.labelLarge
-                        ?.copyWith(color: context.appTheme.primaryColor),
-                  )),
-              SizedBox(
-                width: context.appTheme.spacing.marginMedium,
-              ),
-              ElevatedButton(
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const Divider(
+                      color: Colors.grey,
+                    );
+                  },
+                ),
+              );
+            }),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: context.appTheme.spacing.marginSmall,
+                      vertical: context.appTheme.spacing.buttonVertical,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
                 onPressed: () {},
-                child: const Text(
-                  '共有する',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+                child: Text(
+                  "削除する",
+                  style: context.textTheme.labelLarge
+                      ?.copyWith(color: context.appTheme.primaryColor),
+                )),
+            SizedBox(
+              width: context.appTheme.spacing.marginMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                '共有する',
+                style: TextStyle(
+                  color: Colors.white,
                 ),
               ),
-              SizedBox(
-                width: context.appTheme.spacing.marginMedium,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  '閲覧する',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+            ),
+            SizedBox(
+              width: context.appTheme.spacing.marginMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                '閲覧する',
+                style: TextStyle(
+                  color: Colors.white,
                 ),
               ),
-              SizedBox(
-                width: context.appTheme.spacing.marginMedium,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  '印刷する',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+            ),
+            SizedBox(
+              width: context.appTheme.spacing.marginMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                '印刷する',
+                style: TextStyle(
+                  color: Colors.white,
                 ),
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
