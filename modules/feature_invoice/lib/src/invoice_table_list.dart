4c2047062c31bc6f9ceef7552b1b2b56ed136342
Oeng Mengthong <mengthong.oeng@jaksmok.com>
2024-10-08 18:05:19 +0700

    adjust width for subject column

--- modules/feature_invoice/lib/src/invoice_table_list.dart
+++ modules/feature_invoice/lib/src/invoice_table_list.dart
@@ -38,7 +38,7 @@ class _InvoiceTableListState extends State<InvoiceTableList> {
               Expanded(child: Text('書類番号')), //Document Number
               Expanded(child: SizedBox(width: 100)),
               Expanded(child: Text('宛先')), // address
-              Expanded(flex: 2, child: Text('タイトル')), // title
+              Expanded(flex: 3, child: Text('タイトル')), // title
               Expanded(child: Text('発行日')), // Issue date
               Expanded(child: Text('見込み')), // Prospects
               Expanded(child: Text('売上管理')), // Sales Management
@@ -158,7 +158,7 @@ class _InvoiceTableListState extends State<InvoiceTableList> {
                                 ),
                               ),
                               Expanded(
-                                flex: 2,
+                                flex: 3,
                                 child: Text(
                                   data?.subject ?? '',
                                   style: context.textTheme.bodyMedium,
