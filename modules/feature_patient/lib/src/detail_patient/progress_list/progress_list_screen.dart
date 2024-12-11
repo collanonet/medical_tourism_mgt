cfe993e2d5d418131bfa753b3428bf2d45ad8cf8
Oeng Mengthong <mengthongoenggl@gmail.com>
2024-12-11 13:15:30 +0700

    change text to 訪日検診の流れ

--- modules/feature_patient/lib/src/detail_patient/progress_list/progress_list_screen.dart
+++ modules/feature_patient/lib/src/detail_patient/progress_list/progress_list_screen.dart
@@ -187,7 +187,7 @@ class _ProgressListScreenState extends State<ProgressListScreen> {
                 ),
             children: [
               Text(
-                '訪日再生医療の流れ',
+                '訪日検診の流れ',
                 style: Theme.of(context).textTheme.titleMedium,
               ),
               Row(
