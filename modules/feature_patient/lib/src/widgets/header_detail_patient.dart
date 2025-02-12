d4cff18562cf8d1a0380eb8b6be7138020f69780
HeYi20032804 <heyi089758572@gmail.com>
2025-02-12 13:37:00 +0700

    Update patient head

--- modules/feature_patient/lib/src/widgets/header_detail_patient.dart
+++ modules/feature_patient/lib/src/widgets/header_detail_patient.dart
@@ -14,189 +14,194 @@ class HeaderDetailPatient extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(context.appTheme.spacing.marginMedium),
-      decoration: BoxDecoration(
-        borderRadius:
-            BorderRadius.circular(context.appTheme.spacing.borderRadiusMedium),
-        color: Colors.white,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          // Container(
-          //   height: 60,
-          //   width: 60,
-          //   decoration: BoxDecoration(
-          //     borderRadius: BorderRadius.circular(16),
-          //     color: Colors.grey.shade400,
-          //   ),
-          //   child: const Icon(
-          //     Icons.person,
-          //     color: Colors.white,
-          //   ),
-          // ),
-          // SizedBox(
-          //   width: context.appTheme.spacing.marginMedium,
-          // ),
-          ValueListenableBuilder(
-              valueListenable: context.read<DetailPatientModel>().patientNames,
-              builder: (context, value, _) {
-                return Skeletonizer(
-                  enabled: value.loading,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                              '${value.data?.firstNameRomanized ?? '--'} ${value.data?.middleNameRomanized ?? '--'} ${value.data?.familyNameRomanized ?? '--'}'),
-                          // SizedBox(
-                          //   width: context.appTheme.spacing.marginMedium,
-                          // ),
-                          // Icon(
-                          //   Icons.star,
-                          //   color: Colors.orange,
-                          // ),
-                          // SizedBox(
-                          //   width: context.appTheme.spacing.marginSmall,
-                          // ),
-                          // Container(
-                          //   padding: EdgeInsets.all(
-                          //       context.appTheme.spacing.marginSmall),
-                          //   decoration: BoxDecoration(
-                          //     borderRadius: BorderRadius.circular(4),
-                          //     color: Colors.red,
-                          //   ),
-                          //   child: Text(
-                          //     '出国報告書対象者',
-                          //     style: context.textTheme.bodySmall?.copyWith(
-                          //       color: Colors.white,
-                          //     ),
-                          //   ),
-                          // )
-                        ],
-                      ),
-                      SizedBox(
-                        height: context.appTheme.spacing.marginSmall,
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                           Text('${value.data?.firstNameChineseOrVietnamese ?? '--'} ${value.data?.middleNameChineseOrVietnamese ?? '--'} ${value.data?.familyNameChineseOrVietnamese ?? '--'}'),
-                          SizedBox(
-                            width: context.appTheme.spacing.marginMedium,
+    return ValueListenableBuilder(
+      valueListenable: context.read<DetailPatientModel>().patientData,
+      builder: (context, value, _) {
+        return Skeletonizer(
+          enabled: value.loading,
+          child: Container(
+            padding: EdgeInsets.all(context.appTheme.spacing.marginMedium),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                  context.appTheme.spacing.borderRadiusMedium),
+              color: Colors.white,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // Container(
+                //   height: 60,
+                //   width: 60,
+                //   decoration: BoxDecoration(
+                //     borderRadius: BorderRadius.circular(16),
+                //     color: Colors.grey.shade400,
+                //   ),
+                //   child: const Icon(
+                //     Icons.person,
+                //     color: Colors.white,
+                //   ),
+                // ),
+                // SizedBox(
+                //   width: context.appTheme.spacing.marginMedium,
+                // ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                            '${value.data?.firstNameRomanized ?? '--'} ${value.data?.middleNameRomanized ?? '--'} ${value.data?.familyNameRomanized ?? '--'}'),
+                        // SizedBox(
+                        //   width: context.appTheme.spacing.marginMedium,
+                        // ),
+                        // Icon(
+                        //   Icons.star,
+                        //   color: Colors.orange,
+                        // ),
+                        // SizedBox(
+                        //   width: context.appTheme.spacing.marginSmall,
+                        // ),
+                        // Container(
+                        //   padding: EdgeInsets.all(
+                        //       context.appTheme.spacing.marginSmall),
+                        //   decoration: BoxDecoration(
+                        //     borderRadius: BorderRadius.circular(4),
+                        //     color: Colors.red,
+                        //   ),
+                        //   child: Text(
+                        //     '出国報告書対象者',
+                        //     style: context.textTheme.bodySmall?.copyWith(
+                        //       color: Colors.white,
+                        //     ),
+                        //   ),
+                        // )
+                      ],
+                    ),
+                    SizedBox(
+                      height: context.appTheme.spacing.marginSmall,
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                            '${value.data?.firstNameChineseOrVietnamese ?? '--'} ${value.data?.middleNameChineseOrVietnamese ?? '--'} ${value.data?.familyNameChineseOrVietnamese ?? '--'}'),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginMedium,
+                        ),
+                        Text(
+                            '${value.data?.firstNameJapaneseForChinese ?? '--'} ${value.data?.middleNameJapaneseForChinese ?? '--'} ${value.data?.familyNameJapaneseForChinese ?? '--'}'),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginMedium,
+                        ),
+                        Text(
+                            '${value.data?.firstNameJapaneseForNonChinese ?? '--'} ${value.data?.middleNameJapaneseForNonChinese ?? '--'} ${value.data?.familyNameJapaneseForNonChinese ?? '--'}'),
+                      ],
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        const Text('種別'),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginSmall,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff53A6FF),
                           ),
-                          Text('${value.data?.firstNameJapaneseForChinese ?? '--'} ${value.data?.middleNameJapaneseForChinese ?? '--'} ${value.data?.familyNameJapaneseForChinese ?? '--'}'),
-                          SizedBox(
-                            width: context.appTheme.spacing.marginMedium,
+                          onPressed: () {},
+                          child: Text(value.data!.type?.first ?? '--'),
+                        ),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginMedium,
+                        ),
+                        // const Text('ビザ'),
+                        // SizedBox(
+                        //   width: context.appTheme.spacing.marginSmall,
+                        // ),
+                        // ElevatedButton(
+                        //   style: ElevatedButton.styleFrom(
+                        //     backgroundColor: const Color(0xffDBE238),
+                        //   ),
+                        //   onPressed: () {},
+                        //   child: const Text('医療ビザ'),
+                        // ),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginMedium,
+                        ),
+                        const Text('進捗'),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginSmall,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xffB76D0F),
                           ),
-                          Text('${value.data?.firstNameJapaneseForNonChinese ?? '--'} ${value.data?.middleNameJapaneseForNonChinese ?? '--'} ${value.data?.familyNameJapaneseForNonChinese ?? '--'}'),
-
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }),
-          const Spacer(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  const Text('種別'),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginSmall,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xff53A6FF),
+                          onPressed: () {},
+                          child: Text(value.data?.progress ?? '--'),
+                        ),
+                      ],
                     ),
-                    onPressed: () {},
-                    child: const Text('治療'),
-                  ),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginMedium,
-                  ),
-                  const Text('ビザ'),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginSmall,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xffDBE238),
+                    SizedBox(
+                      height: context.appTheme.spacing.marginMedium,
                     ),
-                    onPressed: () {},
-                    child: const Text('医療ビザ'),
-                  ),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginMedium,
-                  ),
-                  const Text('進捗'),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginSmall,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xffB76D0F),
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  context.appTheme.spacing.marginMedium),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(
+                              color: context.appTheme.primaryColor,
+                            ),
+                          ),
+                          child: Text('GP',
+                              style: context.textTheme.bodySmall?.copyWith(
+                                color: context.appTheme.primaryColor,
+                              )),
+                        ),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginSmall,
+                        ),
+                        Text(value.data?.proposalNumber ?? '--'),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginExtraLarge,
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  context.appTheme.spacing.marginMedium),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(
+                              color: context.appTheme.primaryColor,
+                            ),
+                          ),
+                          child: Text('進捗',
+                              style: context.textTheme.bodySmall?.copyWith(
+                                color: context.appTheme.primaryColor,
+                              )),
+                        ),
+                        SizedBox(
+                          width: context.appTheme.spacing.marginSmall,
+                        ),
+                        const Text('見積&病院提案'),
+                      ],
                     ),
-                    onPressed: () {},
-                    child: const Text('受注'),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: context.appTheme.spacing.marginMedium,
-              ),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: context.appTheme.spacing.marginMedium),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      border: Border.all(
-                        color: context.appTheme.primaryColor,
-                      ),
-                    ),
-                    child: Text('GP',
-                        style: context.textTheme.bodySmall?.copyWith(
-                          color: context.appTheme.primaryColor,
-                        )),
-                  ),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginSmall,
-                  ),
-                  const Text('001-C-20'),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginExtraLarge,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: context.appTheme.spacing.marginMedium),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      border: Border.all(
-                        color: context.appTheme.primaryColor,
-                      ),
-                    ),
-                    child: Text('進捗',
-                        style: context.textTheme.bodySmall?.copyWith(
-                          color: context.appTheme.primaryColor,
-                        )),
-                  ),
-                  SizedBox(
-                    width: context.appTheme.spacing.marginSmall,
-                  ),
-                  const Text('見積&病院提案'),
-                ],
-              ),
-            ],
-          )
-        ],
-      ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
